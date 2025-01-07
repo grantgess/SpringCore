a51c94a8d06b63c2ff1755b65a69f33d94ea6192
grantgess <hhh.kqw146@gmail.com>
2025-01-08 00:07:46 +0300

    CRUD приложение: поулчаем данные из списка people

---
+++ Module2/Task6/src/main/java/org/example/models/Person.java
@@ -0,0 +1,27 @@
+package org.example.models;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
