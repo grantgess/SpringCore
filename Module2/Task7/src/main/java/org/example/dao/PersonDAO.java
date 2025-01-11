b81ac136cca3c62cfbcb629883dd99bc429d3612
grantgess <hhh.kqw146@gmail.com>
2025-01-11 15:27:16 +0300

    добавление объекта в список people используя аннотацию @ModelAttribute

---
+++ Module2/Task7/src/main/java/org/example/dao/PersonDAO.java
@@ -0,0 +1,32 @@
+package org.example.dao;
+
+import org.example.models.Person;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    List<Person> people;
+    private static int COUNTER;
+
+    {
+        people = new ArrayList<Person>();
+        people.add(new Person("Tom", ++COUNTER));
+        people.add(new Person("John", ++COUNTER));
+        people.add(new Person("Bob", ++COUNTER));
+        people.add(new Person("Mike", ++COUNTER));
+    }
+    public List<Person> index() {
+        return people;
+    }
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+    public void save(Person person) {
+        person.setId(++COUNTER);
+        people.add(person);
+    }
+}
