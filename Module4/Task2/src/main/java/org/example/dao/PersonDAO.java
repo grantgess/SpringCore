7dfaeec65196705226f45a731e69892bfeb184b2
grantgess <hhh.kqw146@gmail.com>
2025-01-18 20:10:04 +0300

    валидация email и валидация паттернов

---
+++ Module4/Task2/src/main/java/org/example/dao/PersonDAO.java
@@ -0,0 +1,102 @@
+package org.example.dao;
+
+
+import org.example.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PersonDAO {
+
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index()  {
+        return jdbcTemplate.query("select * from Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("select * from Person where id = ?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
+    }
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM person WHERE email = ?", new Object[]{email},
+                new BeanPropertyRowMapper<Person>(Person.class) {}).stream().findAny();
+    }
+    public void save(Person person) {
+       jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES( ?, ?, ?, ?)", person.getName(), person.getAge(),
+               person.getEmail(), person.getAddress());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? address=? WHERE id=?",
+                updatedPerson.getName(), updatedPerson.getAge(),updatedPerson.getEmail(), updatedPerson.getAddress(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000people();
+        long start = System.currentTimeMillis();
+        for (Person person : people) {
+            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?, ?)",
+                    person.getId(),
+                    person.getName(),
+                    person.getAge(),
+                    person.getEmail(),
+                    person.getAddress());
+
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000people();
+        long start = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES(?, ?, ?, ?, ?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                preparedStatement.setInt(1, people.get(i).getId());
+                preparedStatement.setString(2, people.get(i).getName());
+                preparedStatement.setInt(3, people.get(i).getAge());
+                preparedStatement.setString(4, people.get(i).getEmail());
+                preparedStatement.setString(5, people.get(i).getAddress());
+
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            }
+        });
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+
+    }
+    public List<Person> create1000people() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "Name"+i, 30, "test"+i+"@test.ru", "Russia, Moscow, 123456"));
+        }
+        return people;
+    }
+}
