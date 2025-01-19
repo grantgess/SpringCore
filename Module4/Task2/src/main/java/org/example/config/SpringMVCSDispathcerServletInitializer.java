e2d8b17110de18034ced11b080b6f68934dddd4b
grantgess <hhh.kqw146@gmail.com>
2025-01-19 18:23:36 +0300

    валидация email и валидация паттернов, Выпадающий список

--- Module4/Task2/src/main/java/org/example/config/SpringMVCSDispathcerServletInitializer.java
+++ Module4/Task2/src/main/java/org/example/config/SpringMVCSDispathcerServletInitializer.java
@@ -1,10 +1,14 @@
 package org.example.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.util.EnumSet;
 
 public class SpringMVCSDispathcerServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -30,5 +34,13 @@ public class SpringMVCSDispathcerServletInitializer extends AbstractAnnotationCo
     private void registerHiddenFieldFilter(ServletContext aContext) {
         aContext.addFilter("hiddenHttpMethodFilter",
                 new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+    private void registerCharacterEncodingFilter(ServletContext aContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
     }
 }
